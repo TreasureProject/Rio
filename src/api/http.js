942fdc119bceafe42fe6edda69851e43c8240f5d
Nick Fujita <nickfujita@gmail.com>
2022-12-05 16:49:32 +0900

    'lint'

--- src/api/http.js
+++ src/api/http.js
@@ -25,7 +25,7 @@ function handleHTTP(globalArgs, req, res, next, callback, isPost) {
   const providedArgsCount = providedArgs.length;
   for (let i = 0; i < providedArgsCount; i += 1) {
     const providedArg = providedArgs[i];
-    const argValues = {...req.headers, ...(isPost ? req.body : req.query)};
+    const argValues = { ...req.headers, ...(isPost ? req.body : req.query) };
     const value = argValues[providedArg.name] || argValues[providedArg.name.toLowerCase()];
     if (value != null) {
       let validType = null;
