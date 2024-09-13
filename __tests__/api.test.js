b4e68eb7d83836fdbc22edc5add2c593536fee21
Wyatt Mufson <wyatt@ryu.games>
2024-09-13 11:25:58 -0400

    fix test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -136,7 +136,7 @@ afterAll(async () => {
 describe('Using rio.get, rio.put, rio.post, rio.patch, and rio.delete', () => {
   test('Checking endpoints', async () => {
     const { routes } = rio.utils.getEndpoints(app, rio.paths);
-    expect(routes.length).toBe(29);
+    expect(routes.length).toBe(17);
   });
 
   test('Get sum', async () => {
